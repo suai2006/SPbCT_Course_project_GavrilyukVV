b18ac3d3c0cbca8dcf49ae6e8f22fc34e5eeb62c
Вячеслав Гаврилюк <v.gavrilyuk@fortus.pro>
2023-12-06 12:09:51 +0300

    правки

--- src/components/objectmap/Flat.vue
+++ src/components/objectmap/Flat.vue
@@ -1,14 +1,13 @@
 <template>
 <svg
    version="1.0"
-   width="1010"
-   height="780"
    id="svg24"
-   viewBox="0 0 684 566" 
+   width="1010" height="780"
+   viewBox="0 0 760 580" 
    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns="http://www.w3.org/2000/svg"
    xmlns:svg="http://www.w3.org/2000/svg">   
-   <image x="0" y="0" width="1010" height="780" href="@/assets/svg/01_f25-24_3k.svg"></image>
-   <svg x="0" y="0" width="1010" height="780"><slot/></svg>
+   <image x="0" y="0" width="760" height="580" href="@/assets/svg/01_f25-24_3k.svg"></image>
+   <svg x="0" y="0" width="760" height="580"><slot/></svg>
 </svg>
 </template>
