09b1d98a45d6f7238b3f28ec4ae8832b4fb44e98
Вячеслав Гаврилюк <v.gavrilyuk@fortus.pro>
2023-12-07 13:38:26 +0300

    добавлен компонент для отображения графиков/диаграмм

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,32 @@
 <template>  
   <Content :title="title">
     <h3 class="first">Text Container</h3>
+    <Bar id="my-chart-id" :options="chartOptions" :data="chartData"/>
   </Content>
 </template>
 <script>  
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   export default 
   {
     name: 'home',
-    components: {},
+    components: { Bar },
     data()
     {
-      let obj = {
-        title : "Главная страница"
+      let obj = 
+      {
+        title : "Главная страница",
+        chartData: 
+        {
+          labels: [ 'Январь', 'Февраль', 'Март' ],
+          datasets: [ {label: 'Data One', data: [40, 20, 12] } ]
+        },
+        chartOptions: 
+        {
+            responsive: true
+        }
       };
       return obj;
     },
