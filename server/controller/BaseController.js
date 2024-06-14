5a6c0ac83d7a0d42a4d5d6c6bd9f6abfa1d336dc
Вячеслав Гаврилюк <vvgavrilyuk@hotmail.com>
2024-06-14 15:01:24 +0300

    Правка в базовом контролере

--- server/controller/BaseController.js
+++ server/controller/BaseController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+const telegram = require('telegram');
 class BaseController
 {
     query(query)
@@ -10,6 +12,48 @@ class BaseController
             })
         });
     }
+
+    sendNotificationForUser(incedentList)
+    {
+        if(!incedentList.length) return;
+        return new Promise((resolve, reject) => 
+        {
+            resolve();
+        });
+    }
+
+    sendMail(message)
+    {
+        return new Promise((resolve, reject) => 
+        {
+            if(!this.emailTransporter) this.emailTransporter = nodemailer.createTransport(this.mailConfig);
+            
+            let mailOptions = {
+                from: config.EMAIL.from,                    
+                to: config.EMAIL.to,
+                subject: config.EMAIL.subject,
+                html: message               
+            };
+
+            this.emailTransporter.verify((err, success) =>
+            {
+                if (err) reject(err);
+                this.emailTransporter.sendMail(mailOptions, (error, info) => 
+                {
+                    let success = false;
+                    if(error) resolve({success});
+                    success = true;
+                    resolve({success});
+                });
+            });
+        });
+    }
+
+    sendTelegramm()
+    {
+
+    }
+
 }
 
 module.exports = BaseController;
